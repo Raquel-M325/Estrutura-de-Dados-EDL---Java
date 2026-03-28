f0c5c362c5abb6501474b61284dfc64c551700c8
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-03-28 19:40:47 -0300

    fiz os testes

--- TestePilha.java
+++ TestePilha.java
@@ -1,9 +1,9 @@
 public class TestePilha{
 
     public static void main(String[] args){
-        int quantidade = 100;
+        int quantidade = 1000000;
         System.out.println("Quantidade de elementos: " + (quantidade));
-        PilhaArray arr = new PilhaArray(1,10);
+        PilhaArray arr = new PilhaArray(1,0);
         System.out.println("inserindo");
         long inicio = System.currentTimeMillis();
         for (int i = 0; i < quantidade; i++){
@@ -14,11 +14,6 @@ public class TestePilha{
 
         long fim = System.currentTimeMillis();
         System.out.println("Tempo: " + (fim - inicio) + "ms");
-        System.out.println("retirando");
-        for (int i = 0; i < quantidade; i++){
-            System.out.print(i); //não quebra a linha
-            System.out.println(" - " +arr.pop()); //quebra linha
-        }
 
     }
 }
