21bf5f86628e2f0dfaf2dc50a5eb4c3a4e81c60c
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-03-28 18:04:36 -0300

    agora vai começar os testes

--- TestePilha.java
+++ TestePilha.java
@@ -1,17 +1,21 @@
 public class TestePilha{
 
     public static void main(String[] args){
-        Integer[] novo_arr = new Integer[1];
+        int quantidade = 10;
+        
         PilhaArray arr = new PilhaArray(1,0);
         System.out.println("inserindo");
-        for (int i = 0; i < 10; i++){
+        long inicio = System.currentTimeMillis();
+        for (int i = 0; i < quantidade; i++){
             System.out.println(i);
-            arr.push(new Integer(i));
+            arr.push((i));
 
         }
 
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo: " + (fim - inicio) + "ms");
         System.out.println("retirando");
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < quantidade; i++){
             System.out.print(i); //não quebra a linha
             System.out.println(" - " +arr.pop()); //quebra linha
         }
