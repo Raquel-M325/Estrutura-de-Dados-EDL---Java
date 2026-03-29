6419378bd7c2017cb1c289d7f787d8fd9cad9cfa
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-03-29 15:20:52 -0300

    falta só ajeitar o arquivo de teste para fazer

--- Projeto_das_Pilhas/PilhaArrayRP.java
+++ Projeto_das_Pilhas/PilhaArrayRP.java
@@ -14,11 +14,19 @@ public class PilhaArrayRP implements PilhaRP{
     }
 
     public void push_R(Object o){
+        if (top_R + 1 == top_P){ //está inserindo o vermelho, então só ele vai andar e não o preto
+            grow();
+        }
 
+        arr[++top_R] = o;
     }
 
     public void push_P(Object o){
+        if (top_P - 1 == top_R){ //já aqui é somente o preto, andando o contrário
+            grow();
+        }
 
+        arr[--top_P] = o;
     }
 
     public Object pop_P() throws PilhaVaziaExcecaoRP{
@@ -54,18 +62,45 @@ public class PilhaArrayRP implements PilhaRP{
     }
 
     public int size(){
-
+        return (top_R + 1) + (capacidade - top_P);
     }
 
     public boolean isEmpty(){
+        if (top_R == -1 && top_P == capacidade){
+            return true;
+        }
 
+        return false;
     }
 
     public void empty(){
-        
+        if (isEmpty()){
+            System.out.println("Já está vazio a pilha");
+        }
+
+        top_P = -1;
+        top_R = capacidade;  //preciso ver se funciona dessa forma
     }
 
     public void grow(){
+        int novo_capacidade = capacidade * 2;
+        Object novo_arr[] = new Object[novo_capacidade];
+
+        for (int i = 0; i <= top_R; i++){
+            novo_arr[i] = arr[i]; //copiando o vermelho
+        } 
+
+        int tamanho_P = capacidade - top_P;
+        int novo_top_P = novo_capacidade - tamanho_P;
+
+        for (int i = 0; i < tamanho_P; i++){
+            novo_arr[novo_top_P + i] = arr[top_P + i]; //precisa andar pelo indice somando, sem ser por 1
+        }
+
+
+        capacidade = novo_capacidade;
+        arr = novo_arr;
+        top_P = novo_top_P;
 
     }
 
