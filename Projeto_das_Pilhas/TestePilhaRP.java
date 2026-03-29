13aa9e504706f63822fe6f510eacac938980aa33
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-03-29 15:55:14 -0300

    fazendo aos poucos

--- Projeto_das_Pilhas/TestePilhaRP.java
+++ Projeto_das_Pilhas/TestePilhaRP.java
@@ -1,5 +1,27 @@
 package Projeto_das_Pilhas;
 
 public class TestePilhaRP {
-    
+    public static void main(String[] args){
+        int quantidade = 10; //pode mudar a quantidade de elementos
+        System.out.println("Quantidade de elementos: " + (quantidade));
+        PilhaArrayRP arr = new PilhaArrayRP(0, 0, quantidade * 2);
+        System.out.println("inserindo...");
+
+        long inicio = System.currentTimeMillis();
+        for (int i = 0; i < quantidade; i++){
+            arr.push_P(i); //analisar sobre essa parte melhor
+            arr.push_R(i);
+        }
+
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo do inserção: " + (fim - inicio) + "ms");
+
+        System.out.println("Removendo elementos...");
+        inicio = System.currentTimeMillis();
+        
+        for (int i = 0; i < quantidade; i++){
+            arr.pop_P();
+            arr.pop_R();
+        }
+    }
 }
