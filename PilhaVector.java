9547c6fc76c53086db5d3e496e2db57a7c35d9fb
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-04-04 20:44:44 -0300

    questao 6 feito

---
+++ PilhaVector.java
@@ -0,0 +1,41 @@
+import java.util.Vector;
+
+public class PilhaVector implements Pilha{
+    private Vector<Object> lista = new Vector<>();
+
+    public void push(Object o){
+        lista.add(o);
+    }
+
+    public Object pop() throws PilhaVaziaExcecao{
+        if (isEmpty()){
+            throw new PilhaVaziaExcecao("Pilha está vazia");
+        }
+
+        return lista.remove(lista.size() - 1); //ir no último
+    }
+
+    public Object top() throws PilhaVaziaExcecao{
+        if (isEmpty()) {
+            throw new PilhaVaziaExcecao("Pilha está vazia");
+        }
+
+        return lista.lastElement();
+    }
+
+    public boolean isEmpty(){
+        return lista.isEmpty();
+    }
+    
+    public void empty() throws PilhaVaziaExcecao{
+        if (isEmpty()){
+            throw new PilhaVaziaExcecao("A pilha já está vazia");
+        }
+
+        lista.clear();
+    }
+
+    public int size(){
+        return lista.size();
+    }
+}
