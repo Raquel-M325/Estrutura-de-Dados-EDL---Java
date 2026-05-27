a29400d8838973a3c1941e5612ded57d12eed200
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-05-26 21:07:01 -0300

    correcao sobre root

--- Arvore/No.java
+++ Arvore/No.java
@@ -1,25 +1,26 @@
 package Arvore;
-
 import java.util.ArrayList;
-
 public class No {
     private Object elementos;
-    private No parents, root;
-    private ArrayList<No> children;
+    private No parents;
+    private ArrayList<No> children; //porque tem varios filhos, sendo uma lista de Nós
 
     public No(Object elementos){
         this.elementos = elementos;
         this.parents = null;
-        this.root = null;
         this.children = new ArrayList<No>();
     }
 
-    public Object getElement(Object elementos){
+    public Object getElement(){
         return elementos;
     }
 
-    public No getParent(No parents){
+    public No getParent(){
         return parents;
+    }
+
+    public ArrayList<No> getChildren(){
+        return children;
     }
 
     public void setElement(Object elementos){
@@ -30,4 +31,7 @@ public class No {
         this.parents = parents;
     }
 
+    public void setChildren(ArrayList<No> children){
+        this.children = children;
+    }
 }
