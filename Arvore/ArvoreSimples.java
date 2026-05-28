6c113cf32fd3a65a67bd153dd4680cde7311c62a
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-05-28 00:09:03 -0300

    quase finalizando, só entendendo o pos e pre ordem nos metodos e funcionamento

--- Arvore/ArvoreSimples.java
+++ Arvore/ArvoreSimples.java
@@ -62,28 +62,59 @@ public class ArvoreSimples implements Position{
         }
 
         if (isExternal(node)){
-            return 0; //estagio inicial
+            return 0; //estagio inicial, ele checa na raiz até chegar o final dos filhos, se for externo, ele zera 
+        }
+
+        int maxHeight = 0;
+        for (int i = 0; i < node.getChildren().size(); i++){ 
+            int conta = height(node.getChildren().get(i)); //para ele andar em cada nó dos filhos antes de comparar, esse height fica chamando a recursividade, conferindo varias vezes; esse get serve para pegar a posicao, do qual tive que definir o i para ele andar em cada nó dos filhos
+            maxHeight = Math.max(conta, maxHeight); //compara o valor atual i com o maximo, e retorna o maior
+        }
+
+        return 1 + maxHeight; //soma 1 depois que viu quem é maior, ou seja, o mais profundo
+    }
+
+    public String preorderPrint(No node) throws InvalidPositionExceptionArvore{
+        if (isEmpty()){
+            throw new InvalidPositionExceptionArvore("Erro: Não há nós para acessar");
+        }
+
+        if (node == null){
+            throw new InvalidPositionExceptionArvore("Erro: Nó nulo");
+        }
+
+        if (isRoot(node)){
+            return node.getElement().toString();
+        }
+
+        String resultado = node.getElement().toString(); //pega o elemento do nó atual e transforma em string
+        for (int i = 0; i < node.getChildren().size(); i++){
+            resultado += ", " + preorderPrint(node.getChildren().get(i)); //para cada filho que andar e pegar, vai colocar apos do no atual, sendo no proximo
         }
         
-        int h = 0;
-        for (int i = 0; i < node.getChildren().size(); i++){
-            h = Math.max(h, height);
-        }
-
-        return 1 + h;
-
+        return resultado;
     }
 
-    public String preorderPrint() throws InvalidPositionExceptionArvore{
+    public String posorderPrint(No node) throws InvalidPositionExceptionArvore{
         if (isEmpty()){
             throw new InvalidPositionExceptionArvore("Erro: Não há nós para acessar");
         }
-    }
 
-    public String posorderPrint() throws InvalidPositionExceptionArvore{
-        if (isEmpty()){
-            throw new InvalidPositionExceptionArvore("Erro: Não há nós para acessar");
+        if (node == null){
+            throw new InvalidPositionExceptionArvore("Erro: Nó nulo");
         }
+
+        if (isExternal(node)){
+            return node.getElement().toString();
+        }
+
+        resultado =
+        for (int i = 0; i < node.getParents().getChildren().size(); i++){
+
+        }
+
+        return 
+
     }
 
     public int size(){
--- Arvore/Position.java
+++ Arvore/Position.java
@@ -3,9 +3,9 @@ public interface Position{
     void replace(No no, Object o) throws InvalidPositionExceptionArvore;
     Object swapElement(No nodeA, No nodeB) throws InvalidPositionExceptionArvore;
     int depth(No no) throws InvalidPositionExceptionArvore;
-    int height() throws InvalidPositionExceptionArvore;
-    String preorderPrint() throws InvalidPositionExceptionArvore;
-    String posorderPrint() throws InvalidPositionExceptionArvore;
+    int height(No node) throws InvalidPositionExceptionArvore;
+    String preorderPrint(No node) throws InvalidPositionExceptionArvore;
+    String posorderPrint(No node) throws InvalidPositionExceptionArvore;
     int size();
     boolean isEmpty();
     boolean isRoot(No node) throws InvalidPositionExceptionArvore;
